2b4efede2679bae4d833929b0d5a8a02fdc9ac7d
Melksedeque <freelancer@melksedeque.com.br>
2025-01-29 19:45:12 -0300

    Teste e explicação inicial sobre o que é o useState()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [nome, setNome] = useState("Melk");
+
+  function alterarNome() {
+    setNome("Melksedeque de Souza Silva");
+    console.log(nome);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{nome}</h1>
+        <button onClick={alterarNome}>Mudar para nome completo</button>
       </header>
     </div>
   );
